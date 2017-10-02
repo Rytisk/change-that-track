161fb3feb5855170b03e259bd0c1d3cd6790c493
Rytisk <rytkaplunas@gmail.com>
2017-10-02 20:45:05 +0300

    Added animations and song info

--- app/src/main/java/com/ctt/changethattrack/MainActivity.java
+++ app/src/main/java/com/ctt/changethattrack/MainActivity.java
@@ -4,30 +4,41 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView _play;
-    private ImageView _pause;
-    private ImageView _next;
-    private ImageView _previous;
-    private ImageView _shuffle;
-    private ImageView _repeat;
+    private ImageButton _play;
+    private ImageButton _pause;
+    private ImageButton _next;
+    private ImageButton _previous;
+    private ImageButton _shuffle;
+    private ImageButton _repeat;
+    private TextView _songInfo;
 
-    private String ip = "http://10.0.2.2";
+    private String ip = "http://192.168.1.9";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        _play = (ImageView)findViewById(R.id.img_play);
-        _next = (ImageView)findViewById(R.id.img_next);
-        _previous = (ImageView)findViewById(R.id.img_prev);
-        _shuffle = (ImageView)findViewById(R.id.img_shuffle);
-        _repeat = (ImageView)findViewById(R.id.img_repeat);
+        _play = (ImageButton)findViewById(R.id.img_play);
+        _next = (ImageButton)findViewById(R.id.img_next);
+        _previous = (ImageButton)findViewById(R.id.img_prev);
+        _shuffle = (ImageButton)findViewById(R.id.img_shuffle);
+        _repeat = (ImageButton)findViewById(R.id.img_repeat);
+        _songInfo = (TextView)findViewById(R.id.txt_song_info);
+
+        updateSongInfo();
 
         _play.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 HTTP task = new HTTP();
                 task.execute(ip + ":38475/?action=player_next");
+                updateSongInfo();
             }
         });
 
@@ -50,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 HTTP task = new HTTP();
                 task.execute(ip + ":38475/?action=player_prevous");
+                updateSongInfo();
             }
         });
 
@@ -68,6 +81,24 @@ public class MainActivity extends AppCompatActivity {
                 task.execute(ip + ":38475/?action=set_player_status&statusType=repeat&value=1");
             }
         });
+    }
 
+    void updateSongInfo()
+    {
+        HTTP task = new HTTP();
+        String songInfo = null;
+        try {
+            String response = task.execute(ip + ":38475/?action=get_song_current").get();
+            JSONObject obj = new JSONObject(response);
+            songInfo = obj.getString("PlayingFileName");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        _songInfo.setText(songInfo);
     }
 }
